01fc6a18760aa38fb4f155556a8df097b0e8b0ae
marciodreis6 <marciodreis6@gmail.com>
2026-04-23 11:19:23 -0300

    Update background image and improve styling

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def get_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img = get_base64("fundo.jpg")
+img = get_base64("fundo1.png")
 
 st.markdown(
     f"""
